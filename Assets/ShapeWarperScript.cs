59f39770bd7567080613c3a885920e08612cf57d
panouden <Gimattiolo@gmail.com>
2016-12-28 13:08:03 +0100

    Added Shape warper script and shader
    Added ShapeWarperScript and ShapeWarperShader to enable transition
    between 2 game object geometries. One object goes linearly from original
    shape to sphere of decreasing radius(from radius to 0). The other one
    behaves the other way around.

---
+++ Assets/ShapeWarperScript.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class ShapeWarperScript : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+		done = false;
+		startTime = -1;
+		shapeWarpShader = Shader.Find("Unlit/ShapeWarperShader");
+
+		if (shapeWarpShader == null) 
+		{
+			return;
+		}
+
+		shapeWarpMaterial = GetComponent<Renderer> ().material;
+
+		if(other != null)
+		{
+			otherShapeWarpMaterial = other.GetComponent<Renderer>().material;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+		if (!done) 
+		{
+			if (Time.time > 5) 
+			{
+				done  = true;
+				StartWarp();
+			}
+		}
+
+
+		float t0 = Mathf.Lerp (Time.time, startTime, Convert.ToSingle (startTime >= 0.0f));
+
+		float alpha = Mathf.Lerp(0.0f, 1.0f, (Time.time - t0) / (0.5f * duration));
+		float r = Mathf.Lerp(radius, 0.0f, alpha);
+
+		shapeWarpMaterial.SetFloat ("_Radius", r);
+		shapeWarpMaterial.SetFloat ("_Alpha", alpha);
+
+		float otherAlpha = Mathf.Lerp(1.0f, 0.0f, (Time.time - (t0 + 0.5f * duration)) / (0.5f * duration));
+		float otherR = Mathf.Lerp(radius, 0.0f, otherAlpha);
+
+		otherShapeWarpMaterial.SetFloat ("_Radius", otherR);
+		otherShapeWarpMaterial.SetFloat ("_Alpha", otherAlpha);
+	}
+
+	public void StartWarp()
+	{
+		startTime = Time.time;
+	}
+
+
+	public GameObject other;
+	public float radius;
+	public float duration;
+
+
+
+	private Shader shapeWarpShader;
+	private Material shapeWarpMaterial;
+	private Material otherShapeWarpMaterial;
+
+
+	private bool done;
+	private float startTime;
+
+}
---
+++ Assets/ShapeWarperShader.shader
@@ -0,0 +1,71 @@
+﻿Shader "Unlit/ShapeWarperShader"
+{
+	Properties
+	{
+		_MainTex("Base (RGB)", 2D) = "white" {}
+		_Alpha("Alpha", Range(0, 1)) = 0
+		_Radius("Radius", Range(0, 10)) = 1
+	}
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" }
+		LOD 100
+
+		Pass
+		{
+			Cull off
+
+			CGPROGRAM
+			#pragma target 3.0
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+
+			struct vIn
+			{
+				float4 vertex : POSITION;
+				float4 normal : NORMAL;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+			};
+
+
+			float _Alpha;
+			float _Radius;
+			
+			sampler2D _MainTex;
+			float4 _MainTex_ST;
+
+			v2f vert (vIn v)
+			{
+				v2f o;
+
+#if !defined(SHADER_API_OPENGL)
+				float3 nV = normalize(v.vertex.xyz);
+				v.vertex.xyz = lerp(v.vertex.xyz, _Radius * nV, _Alpha);
+				v.vertex.w = 1;
+#endif
+
+				o.vertex = mul(UNITY_MATRIX_MVP, v.vertex);
+				//o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+				return o;
+			}
+			
+
+			fixed4 frag (v2f i) : SV_Target
+			{
+				fixed4 col = tex2D(_MainTex, i.uv);
+				//col.a = _Alpha;
+				return col;
+			}
+			ENDCG
+		}
+	}
+}
