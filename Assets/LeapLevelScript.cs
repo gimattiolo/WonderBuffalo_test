09f33158350c9aaf8d9dfab0b3250624f3011f15
mattiolog <giuseppe.mattiolo@technicolor.com>
2017-01-13 17:11:50 -0800

    Added new interaction material and holding pose controller
    Added new interaction material and holding pose controller
    Aligned object to the hand so that object z is aligned to hand direction
    and object x to - hand normal
    Attach object to hand on grab. The attach code is in the new
    LeapLevelScript attached to the LevelObject

---
+++ Assets/LeapLevelScript.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+using Leap.Unity.Interaction;
+
+public class LeapLevelScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        manager = (InteractionManager)FindObjectOfType(typeof(InteractionManager));
+        marker = GameObject.Find("Capsule");
+
+        //IInteractionBehaviour b = marker.GetComponent<IInteractionBehaviour>();
+        //manager.GraspWithHand(manager., marker.GetComponent<InteractionManager>());
+
+        done = false;
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+
+    public void OnHandGrasp(Hand hand)
+    {
+        print("grasped");
+        if (done)
+        {
+            return;
+        }
+        done = true;
+
+        manager.ReleaseHand(hand.Id);
+
+
+
+        //marker.transform.parent = ;
+
+        //ma
+
+        //Vector3 handNormal = new Vector3(h.PalmNormal.x, h.PalmNormal.y, h.PalmNormal.z);
+        //Vector3 handDirection = new Vector3(h.Direction.x, h.Direction.y, h.Direction.z);
+
+        //Vector3 bodyPosition = new Vector3(h.PalmPosition.x, h.PalmPosition.y, h.PalmPosition.z);// + 1.0f * offset;//;_obj.warper.RigidbodyPosition;
+
+        //Quaternion bodyRotation = Quaternion.LookRotation(handDirection, Vector3.Cross(handNormal, handDirection));//= _obj.warper.RigidbodyRotation;
+
+
+        manager.GraspWithHand(hand, marker.GetComponent<IInteractionBehaviour>());
+    }
+    public void OnHandRelease(Hand hand)
+    {
+        print("released");
+        //manager.ReleaseHand(hand.Id);
+
+        //manager.GraspWithHand(hand, marker.GetComponent<IInteractionBehaviour>());
+
+    }
+
+
+    private InteractionManager manager;
+    private GameObject marker;
+
+    bool done;
+}
+    
---
+++ Assets/MyHoldingPoseController.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using Leap.Unity.Interaction;
+using Leap;
+using System.Collections.Generic;
+using LeapInternal;
+using Leap.Unity.Interaction.CApi;
+
+
+/**
+* IHoldingPoseController defines the interface used by the Interaction Engine
+* to request the best pose for an object when it is held.
+*
+* The Interaction Engine provides the HoldingPoseControllerKabsh implementation for
+* this controller.
+* @since 4.1.4
+*/
+public class MyHoldingPoseController : IHoldingPoseController
+{
+    /**
+    * Add the specified hand to the pose calculation.
+    * @param hand The Leap.Hand object containing the reported tracking data.
+    * @since 4.1.4
+    */
+    public override void AddHand(Hand hand)
+    { }
+    /**
+    * Reports that a hand has been re-identified and that you should replace the
+    * old hand with the new hand data.
+    * @param oldId the previous Leap.Hand.Id value
+    * @param newId the replacement Leap.Hand.Id
+    * @since 4.1.4
+    */
+    public override void TransferHandId(int oldId, int newId)
+    { }
+    /**
+    * Remove the specified hand from the pose calculation.
+    * @param hand The Leap.Hand object to be removed.
+    * @since 4.1.4
+    */
+    public override void RemoveHand(Hand hand)
+    { }
+    /**
+    * Calculate the best holding pose given the current state of the hands and 
+    * interactable object.
+    * @param hands the list of hands with the current tracking data.
+    * @param position A Vector3 object to be filled with the disred object position.
+    * @param rotation A Quaternion object to be filled with the desired rotation.
+    * @since 4.1.4
+    */
+    public override void GetHoldingPose(ReadonlyList<Hand> hands, out Vector3 position, out Quaternion rotation)
+    {
+        if(hands.Count <= 0)
+        {
+            //return;
+        }
+
+        Hand h = hands[0];
+
+        Vector3 handNormal = new Vector3(h.PalmNormal.x, h.PalmNormal.y, h.PalmNormal.z);
+        Vector3 handDirection = new Vector3(h.Direction.x, h.Direction.y, h.Direction.z);
+
+        Vector3 bodyPosition = new Vector3(h.PalmPosition.x, h.PalmPosition.y, h.PalmPosition.z);// + 1.0f * offset;//;_obj.warper.RigidbodyPosition;
+
+        Quaternion bodyRotation = Quaternion.LookRotation(handDirection, Vector3.Cross(handNormal, handDirection));//= _obj.warper.RigidbodyRotation;
+        position = bodyPosition;
+        rotation = bodyRotation;
+    }
+}
